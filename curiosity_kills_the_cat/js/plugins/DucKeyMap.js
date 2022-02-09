1bca83215763d9b3c01527961f62a74c46b69765
Duc Ho <hotienduc05012002@gmail.com>
2022-02-09 14:31:52 -0700

    Resolve Conflict

---
+++ curiosity_kills_the_cat/js/plugins/DucKeyMap.js
@@ -0,0 +1,10 @@
+/*:
+ * @plugindesc This plugin ables new key press: v
+ * @author Duc Ho
+ * @help
+ * This script just add a new key into RPG Maker MV, which is V
+ */
+
+(function () {
+  Input.keyMapper[86] = "action"; // keycode for V;
+})();
